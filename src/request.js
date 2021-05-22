75fd96ec76c271f69a4a7b16f1aa70f1c67df2e5
Jayen Patel <pateljayen07@gmail.com>
2021-05-22 09:30:05 +0530

    Updated Commit

--- src/request.js
+++ src/request.js
@@ -4,7 +4,7 @@ const req = () => {
     return new Promise((resolve, reject) => {
         request('https://api.wazirx.com/api/v2/tickers', (err, res, body) => {
             if (err)
-                return console.log("Something went wrong");
+                return reject("Something went wrong");
             const data = JSON.parse(body)
             resolve(data);
         })
